22dfff930e970517fe52639c3b824f42ced3bf60
antigen <antigen@vm.(none)>
2013-04-17 16:48:21 +0600

    rabota vecher

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Depot::Application.routes.draw do
+  get "store/index"
+
   resources :products
 
 
@@ -54,7 +56,8 @@ Depot::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-
+ 
+   root to: 'store#index' , as: 'store' 
 
   # See how all your routes lay out with "rake routes"
 
